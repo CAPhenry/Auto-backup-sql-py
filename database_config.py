8dc228be2b75736c549fb48115c6ac55d8042c70
CAPhenry <henrybotelho2017@outlook.com>
2024-06-10 02:13:06 -0300

    Commit, adicionando arquivos.

---
+++ database_config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+    "host": "localhost", #sua host, o padrão é localhost
+    "user": "root", #nome de usuario do banco de dados, o padrão é ( root )
+    "password": "", #senha (caso tenha)
+    "database": "vampireland", #nome do banco de dados
+    "charset": "utf8",
+    "path": "C:/Users/Administrator/Desktop/backup-sql", #diretorio para pasta de salvamento exemplo: "C:/Users/Administrator/Desktop/backup-sql"
+    "backup_interval": 300, #Tempo do intervalo para fazer backup, 60 = 1 minuto, 300 = 5 minutos
+}
+
+
